d5299b8dffcec40ea42eb11b86355fbc8d45450d
mgmeyers <matthew.gregory.meyers@gmail.com>
2020-11-21 09:03:26 -0800

    Initial commit

---
+++ make.sh
@@ -0,0 +1,1 @@
+nativefier --name "Dynalist" --single-instance --electron-version 11.0.2 --browserwindow-options "{ \"trafficLightPosition\": { \"x\": 20, \"y\": 35 } }" --title-bar-style hidden "https://dynalist.io" 
